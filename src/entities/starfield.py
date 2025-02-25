f336d4954f806c38c8efb24497098431667ceeb1
pseudoDjnn <lavahrexes@gmail.com>
2025-02-25 00:47:25 -0500

    adding a class for the background now

---
+++ src/entities/starfield.py
@@ -0,0 +1,13 @@
+import random
+from utils.constants import *
+
+class Starfield:
+  def __init__(self):
+    self.stars = []
+
+
+  def draw(self, screen):
+    pass
+  
+  def update(self, dt):
+    pass
